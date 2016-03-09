58fde430ad11e3a301e41c157f0fcd5bf0b42d93
Alejandro Granadillo <slayerfat@gmail.com>
2016-03-09 12:14:16 -0430

    Adicion de codigo fuente

---
+++ Interfaces/PhoneParserInterface.php
@@ -0,0 +1,21 @@
+<?php namespace Slayerfat\PhoneParser\Interfaces;
+
+interface PhoneParserInterface
+{
+
+    /**
+     * Chequea y devuelve el telefono acomodado.
+     * ej: 02123334422 -> (212)-333-4422
+     * @param int|string $number el numero, ej: 02123334422
+     * @return string regresa en formato (212)-333-4422
+     */
+    public function parseNumber($number);
+
+    /**
+     * Chequea y devuelve el telefono acomodado.
+     * ej: algo1232224455xzx -> 1232224455
+     * @param string $number el texto con el numero.
+     * @return string regresa el numero, ej: 1232224455
+     */
+    public function parseString($number);
+}
---
+++ PhoneParser.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Slayerfat\PhoneParser;
+
+use Slayerfat\PhoneParser\Interfaces\PhoneParserInterface;
+
+/**
+ * Class PhoneParser
+ *
+ * @package Slayerfat\PhoneParser
+ * @author Alejandro Granadillo <slayerfat@gmail.com>
+ * @link https://github.com/slayerfat/VzlaPhoneParser Repositorio en linea.
+ */
+class PhoneParser implements PhoneParserInterface
+{
+
+    /**
+     * Regular Expression para el numero de telefono con formato Venezolano.
+     * 0123456478
+     *
+     * @var string
+     */
+    private $completePhoneRegex = '/(?P<cero>[0]?)(?P<code>[0-9]{3})(?P<trio>[\d]{3})(?P<gangbang>[\d]{4})/';
+
+    /**
+     * Regular Expression para el numero de telefono con formato no Venezolano.
+     * 01234564789
+     *
+     * @var string
+     */
+    private $rawPhoneRegex = '/(?P<cero>0)?(?P<numbers>\d{10})/';
+
+    /**
+     * Chequea y devuelve el telefono acomodado.
+     * ej: 02123334422 -> (212)-333-4422
+     *
+     * @param int|string $number el numero, ej: 02123334422
+     * @return string regresa en formato (212)-333-4422
+     */
+    public function parseNumber($number)
+    {
+        if (is_null($number)) {
+            return '';
+        }
+
+        $matches = $this->execute($this->completePhoneRegex, $number);
+
+        if (sizeof($matches) > 1) {
+            return "({$matches['code']})-{$matches['trio']}-{$matches['gangbang']}";
+        }
+
+        return '';
+    }
+
+    /**
+     * Ejecuta la comprobacion por medio de regex
+     * y devuelve lo se pudo capturar.
+     *
+     * @param string $regex el regex a ejecutar
+     * @param string $data el texto al que se hara la comprobacion.
+     * @return string[]
+     */
+    private function execute($regex, $data)
+    {
+        $matches = [];
+
+        preg_match($regex, (string)$data, $matches);
+
+        return $matches;
+    }
+
+    /**
+     * Chequea y devuelve el telefono acomodado.
+     * ej: algo1232224455xzx -> 1232224455
+     *
+     * @param string $number el texto con el numero.
+     * @return string regresa el numero, ej: 1232224455
+     */
+    public function parseString($number)
+    {
+        if (is_null($number)) {
+            return '';
+        }
+
+        $matches = $this->execute($this->rawPhoneRegex, $number);
+
+        if (sizeof($matches) > 0) {
+            return $matches['numbers'];
+        }
+
+        return '';
+    }
+}
